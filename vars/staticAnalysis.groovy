cc2f9e00e4d64e242230554329230bd288a527e2
@SanPanda <deivis9010@gmail.com>
2026-02-05 01:37:34 -0300

    Agregar script para análisis estático y evaluación de Quality Gate

---
+++ vars/staticAnalysis.groovy
@@ -0,0 +1,26 @@
+
+def call(Map config = [:]) {
+    
+    def abortPipeline = config.get('abortPipeline', false)
+
+   
+    timeout(time: 5, unit: 'MINUTES') {
+        script {
+            echo "Iniciando análisis estático..."
+            
+            
+            withSonarQubeEnv('SonarQube') { 
+                sh 'echo "Ejecución de las pruebas de calidad de código"'
+            }
+
+            echo "Evaluando Quality Gate..."
+            
+           
+            if (abortPipeline) {
+                error "El Quality Gate ha fallado y abortPipeline es True. Deteniendo ejecución."
+            } else {
+                echo "Quality Gate evaluado. Continuando pipeline (abortPipeline es False)."
+            }
+        }
+    }
+}
